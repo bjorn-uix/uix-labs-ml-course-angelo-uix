1760abf4802a71520755c2be47d4ece571efa5c5
bjorn-uix <106654277+bjorn-uix@users.noreply.github.com>
2023-09-30 12:50:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,5 +41,5 @@ def collaborative_recommend():
         return data
 
 
-#if __name__ == '__main__':
-#    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=4567, debug=True)
