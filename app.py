49c36ca660c08fbf858ae37940d62fa181c92f61
bjorn-uix <106654277+bjorn-uix@users.noreply.github.com>
2023-10-06 17:04:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    data = "Welcome to Instalearn ML Tutorial students!"
+    data = "Welcome to Instalearn ML Tutorial students kfkdfj!"
     return {"message": data}
 
 
